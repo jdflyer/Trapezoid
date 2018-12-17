b0e5daf4a82317858ecf17487b7b95088b1bd2dc
jdflyer <yeacub@gmail.com>
2018-12-16 19:57:18 -0700

    Added plr test movement

--- mainloop.js
+++ mainloop.js
@@ -6,11 +6,21 @@ function MainLoop(){
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     // Clear the color buffer with specified clear color
     gl.clear(gl.COLOR_BUFFER_BIT);
-    DrawQuad([TransX,1,TransX+.25,1,TransX,-1,TransX+.25,-1],[1,1,1,1]);
+    DrawQuad([TransX,TransY,TransX+.25,TransY,TransX,TransY-.5,TransX+.25,TransY-.5],[1,1,1,1]);
     DrawTrapezoidLogo();
-    if (TransX>=1){
+    if (TransX>1){
         TransX=-1.25;
+    }else if(TransX<-1.25){
+        TransX=1;
     }
-    TransX = TransX+0.005;
+    if (rightpressed==true){ //If right arrow is pressed
+        TransX = TransX+0.005;
+    }else if(leftpressed==true){ //If left arrow is pressed
+        TransX = TransX-0.005;
+    }else if (uppressed==true){ //If right arrow is pressed
+        TransY = TransY+0.01;
+    }else if(downpressed==true){ //If left arrow is pressed
+        TransY = TransY-0.01;
+    }
     window.requestAnimationFrame(MainLoop);
 }
--- vars.js
+++ vars.js
@@ -2,3 +2,5 @@ var canvas = document.getElementById("canvas")
 var gl = canvas.getContext("webgl");
 var Ready = false;
 var TransX = -1;
+var TransY = 1;
+var leftpressed,rightpressed,uppressed,downpressed;
